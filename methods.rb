eb4f892209efbbf223df5bce4fe04dd5817bd5f4
Ben Jackson <benjacksonSE@gmail.com>
2021-12-08 22:04:51 -0500

    commit tree 9f53dca67213c496114c172d68b60ef9ba79745b

--- methods.rb
+++ methods.rb
@@ -1,1 +1,35 @@
-# Your code here!
+
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+greet_programmer
+
+def greet name 
+  puts "Hello, " + name + "!"
+end
+
+greet "Ben"
+
+def greet_wtih_default(name = "programmer")
+  puts "Hello, " + name + "!"
+end
+
+greet_wtih_default "Ben"
+greet_wtih_default
+
+def add (num1, num2)
+  num1 + num2
+end
+
+add 2, 45
+
+def halve num 
+  if num / 2 != nil
+    num/2
+  end
+  nil
+end
+
+halve 2
+halve 5
